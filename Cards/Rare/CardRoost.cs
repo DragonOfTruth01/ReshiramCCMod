9675127e479f6fbad584ae201e42059991261d98
Steven Berry <stevenmichaelberry@gmail.com>
2025-10-08 21:31:16 -0700

    Add Roost

---
+++ Cards/Rare/CardRoost.cs
@@ -0,0 +1,115 @@
+﻿using Nickel;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace DragonOfTruth01.ReshiramCCMod.Cards;
+
+internal sealed class CardRoost : Card, ReshiramCCModCard
+{
+    public static void Register(IModHelper helper)
+    {
+        helper.Content.Cards.RegisterCard("Roost", new()
+        {
+            CardType = MethodBase.GetCurrentMethod()!.DeclaringType!,
+            Meta = new()
+            {
+                deck = ModEntry.Instance.ReshiramCCMod_Deck.Deck,
+                rarity = Rarity.rare,
+                upgradesTo = [Upgrade.A, Upgrade.B]
+            },
+            Name = ModEntry.Instance.AnyLocalizations.Bind(["card", "Roost", "name"]).Localize
+        });
+    }
+    public override CardData GetData(State state)
+    {
+        CardData data = new CardData()
+        {
+            cost = upgrade == Upgrade.A ? 1 : 2,
+            exhaust = true
+        };
+        return data;
+    }
+    public override List<CardAction> GetActions(State s, Combat c)
+    {
+        List<CardAction> actions = new();
+
+        switch (upgrade)
+        {
+            case Upgrade.None:
+                actions = new()
+                {
+                    new AHeal()
+                    {
+                        healAmount = 2,
+                        targetPlayer = true
+                    },
+                    new AStatus()
+                    {
+                        status = Status.heat,
+                        statusAmount = -3,
+                        targetPlayer = true
+                    },
+                    new AStatus()
+                    {
+                        status = ModEntry.Instance.Flammable.Status,
+                        statusAmount = 1,
+                        targetPlayer = true
+                    }
+                };
+                break;
+
+            case Upgrade.A:
+                actions = new()
+                {
+                    new AHeal()
+                    {
+                        healAmount = 2,
+                        targetPlayer = true
+                    },
+                    new AStatus()
+                    {
+                        status = Status.heat,
+                        statusAmount = -3,
+                        targetPlayer = true
+                    },
+                    new AStatus()
+                    {
+                        status = ModEntry.Instance.Flammable.Status,
+                        statusAmount = 1,
+                        targetPlayer = true
+                    }
+                };
+                break;
+
+            case Upgrade.B:
+                actions = new()
+                {
+                    new AHeal()
+                    {
+                        healAmount = 2,
+                        targetPlayer = true
+                    },
+                    new AStatus()
+                    {
+                        status = Status.tempShield,
+                        statusAmount = 2,
+                        targetPlayer = true
+                    },
+                    new AStatus()
+                    {
+                        status = Status.heat,
+                        statusAmount = -3,
+                        targetPlayer = true
+                    },
+                    new AStatus()
+                    {
+                        status = ModEntry.Instance.Flammable.Status,
+                        statusAmount = 1,
+                        targetPlayer = true
+                    }
+                };
+                break;
+        }
+        return actions;
+    }
+}
--- ModEntry.cs
+++ ModEntry.cs
@@ -69,7 +69,8 @@ public sealed class ModEntry : SimpleMod
     internal static IReadOnlyList<Type> ReshiramCCModCharacter_RareCard_Types { get; } = [
         typeof(CardFusionFlare),
         typeof(CardBlueFlare),
-        typeof(CardDracoMeteor)
+        typeof(CardDracoMeteor),
+        typeof(CardRoost)
     ];
 
     /* We can use an IEnumerable to combine the lists we made above, and modify it if needed
