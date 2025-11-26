48b90f18b23d4bfc436440ad4917c834f1b7fdd2
Steven Berry <stevenmichaelberry@gmail.com>
2025-11-26 13:20:24 -0800

    Add Null Check for Searing Shot

--- Features/CustomActions/ADamageHeatRightmostExhaust.cs
+++ Features/CustomActions/ADamageHeatRightmostExhaust.cs
@@ -10,24 +10,27 @@ public sealed class ADamageHeatRightmostExhaust : CardAction
     public override void Begin(G g, State s, Combat c)
     {
         // Get the rightmost card in the hand
-        Card card = c.hand[c.hand.Count - 1];
+        if(c.hand[c.hand.Count - 1] != null)
+        {
+            Card card = c.hand[c.hand.Count - 1];
 
-        int damageHeatDealt = card.GetCurrentCost(s) + damageHeatMod;
+            int damageHeatDealt = card.GetCurrentCost(s) + damageHeatMod;
 
-        timer = 0.0;
-        c.Queue(new ADelay
-            {
-                timer = 0.5
-            });
-        c.Queue(new AExhaustOtherCard
-            {
-                uuid = card.uuid
-            });
-        c.Queue(new AAttack
-            {
-                damage = Card.GetActualDamage(s, damageHeatDealt, card: null),
-                status = Status.heat,
-                statusAmount = damageHeatDealt
-            });
+            timer = 0.0;
+            c.Queue(new ADelay
+                {
+                    timer = 0.5
+                });
+            c.Queue(new AExhaustOtherCard
+                {
+                    uuid = card.uuid
+                });
+            c.Queue(new AAttack
+                {
+                    damage = Card.GetActualDamage(s, damageHeatDealt, card: null),
+                    status = Status.heat,
+                    statusAmount = damageHeatDealt
+                });
+        }
     }
 }
