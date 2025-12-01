5a117881e06b638019f81def0f24f64d588da14f
Steven Berry <stevenmichaelberry@gmail.com>
2025-11-30 20:28:45 -0700

    Buff Flame Charge B

--- Cards/Common/CardFlameCharge.cs
+++ Cards/Common/CardFlameCharge.cs
@@ -25,7 +25,7 @@ internal sealed class CardFlameCharge : Card, IReshiramCCModCard
         CardData data = new CardData()
         {
             art = ModEntry.Instance.ReshiramCCMod_Character_CardFlameChargeBG.Sprite,
-            cost = upgrade == Upgrade.B ? 1 : 0
+            cost = 0
         };
         return data;
     }
