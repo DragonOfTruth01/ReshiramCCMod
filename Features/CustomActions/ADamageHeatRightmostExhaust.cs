cfd9d0562f494ae62415e3c9a5d687ab85bdf76b
Steven Berry <stevenmichaelberry@gmail.com>
2025-12-22 08:25:32 -0800

    Fix Bug with Gemini Swap

--- Features/CustomActions/ADamageHeatRightmostExhaust.cs
+++ Features/CustomActions/ADamageHeatRightmostExhaust.cs
@@ -18,19 +18,21 @@ public sealed class ADamageHeatRightmostExhaust : CardAction
             int damageHeatDealt = card.GetCurrentCost(s) + damageHeatMod;
 
             timer = 0.0;
-            c.Queue(new ADelay
-                {
-                    timer = 0.5
-                });
-            c.Queue(new AExhaustOtherCard
-                {
-                    uuid = card.uuid
-                });
-            c.Queue(new AAttack
+
+            // Queue actions in inverse order because we're using QueueImmediate
+            c.QueueImmediate(new AAttack
                 {
                     damage = Math.Max(0, damageHeatDealt - 10),
                     status = Status.heat,
                     statusAmount = Math.Max(0, damageHeatDealt - 10)
+                });
+            c.QueueImmediate(new AExhaustOtherCard
+                {
+                    uuid = card.uuid
+                });
+            c.QueueImmediate(new ADelay
+                {
+                    timer = 0.5
                 });
         }
     }
